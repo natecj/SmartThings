1966b6f220289327fa98f13e690c2999e5c659c7
Nathan Jacobson <natecj@gmail.com>
2015-10-17 14:01:13 -0400

    Add Z-Wave Remote Jasco device

---
+++ devicetypes/natecj/zwave-remote-jasco.src/zwave-remote-jasco.groovy
@@ -0,0 +1,113 @@
+/**
+ *  Copyright 2015 Nathan Jacobson
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+metadata {
+	definition (name: "Z-Wave Remote Jasco", namespace: "natecj", author: "Nathan Jacobson") {
+		capability "Actuator"
+		capability "Button"
+		capability "Configuration"
+		capability "Sensor"
+		capability "Battery"
+
+		fingerprint deviceId: "0x01"
+	}
+
+	simulator {
+
+	}
+
+	tiles {
+		standardTile("state", "device.state", width: 2, height: 2) {
+			state "connected", label: "", icon: "st.unknown.zwave.remote-controller", backgroundColor: "#ffffff"
+		}
+
+		valueTile("battery", "device.battery", inactiveLabel: false, decoration: "flat") {
+			state "battery", label:'${currentValue}% battery', unit:""
+		}
+
+		main "state"
+		details(["state", "battery"])
+	}
+}
+
+def parse(String description) {
+	def results = []
+	if (description.startsWith("Err")) {
+	    results = createEvent(descriptionText:description, displayed:true)
+	} else {
+		def cmd = zwave.parse(description, [0x2B: 1, 0x80: 1, 0x84: 1])
+		if(cmd) results += zwaveEvent(cmd)
+		if(!results) results = [ descriptionText: cmd, displayed: false ]
+	}
+	log.debug("Parsed '$description' to $results")
+	return results
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.wakeupv1.WakeUpNotification cmd) {
+	def results = [createEvent(descriptionText: "$device.displayName woke up", isStateChange: false)]
+
+	def prevBattery = device.currentState("battery")
+	if (!prevBattery || (new Date().time - prevBattery.date.time)/60000 >= 60 * 53) {
+		results << response(zwave.batteryV1.batteryGet().format())
+	}
+	results += configurationCmds().collect{ response(it) }
+	results << response(zwave.wakeUpV1.wakeUpNoMoreInformation().format())
+	return results
+}
+
+def buttonEvent(button, held) {
+	button = button as Integer
+	log.debug "buttonEvent: $device.displayName button $button was held ($held)"
+	if (held) {
+		createEvent(name: "button", value: "held", data: [buttonNumber: button], descriptionText: "$device.displayName button $button was held", isStateChange: true)
+	} else {
+		createEvent(name: "button", value: "pushed", data: [buttonNumber: button], descriptionText: "$device.displayName button $button was pushed", isStateChange: true)
+	}
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.sceneactivationv1.SceneActivationSet cmd) {
+	Integer button = ((cmd.sceneId + 1) / 2) as Integer
+	Boolean held = !(cmd.sceneId % 2)
+	buttonEvent(button, held)
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.batteryv1.BatteryReport cmd) {
+	def map = [ name: "battery", unit: "%" ]
+	if (cmd.batteryLevel == 0xFF) {
+		map.value = 1
+		map.descriptionText = "${device.displayName} has a low battery"
+	} else {
+		map.value = cmd.batteryLevel
+	}
+	createEvent(map)
+}
+
+def zwaveEvent(physicalgraph.zwave.Command cmd) {
+	[ descriptionText: "$device.displayName: $cmd", linkText:device.displayName, displayed: false ]
+}
+
+def configurationCmds() {
+	[ zwave.configurationV1.configurationSet(parameterNumber: 250, scaledConfigurationValue: 1).format(),
+	  zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:zwaveHubNodeId).format() ]
+}
+
+def configure() {
+	def cmd = configurationCmds()
+	log.debug("Sending configuration: $cmd")
+	return cmd
+}
+
+def zwaveEvent(physicalgraph.zwave.Command cmd) {
+	// Handles all Z-Wave commands we aren't interested in
+	[:]
+}
