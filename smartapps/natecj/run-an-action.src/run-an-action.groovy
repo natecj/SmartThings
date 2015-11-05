9eff32b70d1f4b40596616db506f7d08d5415537
Nathan Jacobson <natecj@gmail.com>
2015-11-04 22:31:44 -0500

    Add new plugin

---
+++ smartapps/natecj/run-an-action.src/run-an-action.groovy
@@ -0,0 +1,48 @@
+/**
+ *  Copyright 2015 Nathan Jacobson <natecj@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+definition(
+  name: "Run an Action",
+  namespace: "natecj",
+  author: "Nathan Jacobson",
+  description: "",
+  category: "Convenience",
+  iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+  iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
+)
+
+preferences {
+  def actions = location.helloHome?.getPhrases()*.label
+  if (actions) {
+    actions.sort()
+    section("Hello Home Actions") {
+      input "action", "enum", title: "Select an action to execute", options: actions
+    }
+  }
+}
+
+def installed() {
+  log.debug "installed()"
+  initialize()
+}
+
+def updated() {
+  log.debug "updated()"
+  unsubscribe()
+  initialize()
+}
+
+def initialize() {
+  log.debug "initialize()"
+}
