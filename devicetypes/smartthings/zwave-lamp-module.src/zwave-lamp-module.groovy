87337b977606c9739a7f455974f4885257eef1e7
Nathan Jacobson <natecj@gmail.com>
2015-11-15 15:08:17 -0500

    Move SmartThings-based devices to their own namespace

--- devicetypes/smartthings/zwave-lamp-module.src/zwave-lamp-module.groovy
+++ devicetypes/smartthings/zwave-lamp-module.src/zwave-lamp-module.groovy
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2015 Nathan Jacobson
+ *  Copyright 2015 SmartThings
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -12,7 +12,7 @@
  *
  */
 metadata {
-	definition (name: "Z-Wave Lamp Module", namespace: "natecj", author: "Nathan Jacobson") {
+	definition (name: "Z-Wave Lamp Module", namespace: "smartthings", author: "SmartThings") {
 		capability "Switch Level"
 		capability "Actuator"
 		capability "Indicator"
