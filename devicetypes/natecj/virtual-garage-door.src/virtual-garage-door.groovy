711d7cf86d283b049aad6e2e663064f357cfb7e5
Nathan Jacobson <natecj@gmail.com>
2015-11-15 15:24:21 -0500

    Add preferences to device

--- devicetypes/natecj/virtual-garage-door.src/virtual-garage-door.groovy
+++ devicetypes/natecj/virtual-garage-door.src/virtual-garage-door.groovy
@@ -1,5 +1,7 @@
 /**
- *  Copyright 2015 Nathan Jacobson <natecj@gmail.com>
+ *  Combined Garage Door Opener
+ *
+ *  Copyright 2015 Nathan Jacobson
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -12,26 +14,34 @@
  *
  */
 metadata {
-	definition (name: "Virtual Garage Door", namespace: "natecj", author: "Nathan Jacobson") {
+	definition (name: "Combined Garage Door Opener", namespace: "natecj", author: "Nathan Jacobson") {
 		capability "Actuator"
 		capability "Door Control"
     capability "Garage Door Control"
 		capability "Contact Sensor"
 		capability "Refresh"
 		capability "Sensor"
+		capability "Lock"
+		capability "Switch"
+	}
 
-    command "finishOpening"
-		command "finishClosing"
+	simulator {
+
+	}
+
+	preferences {
+		input "deviceButton", "capability.switch", title: "Door Button", description: "The switch/button that opens and closes the door", displayDuringSetup: true
+		input "deviceSensor", "capability.contactSensor", title: "Door Sensor", description: "The sensor that detects if the door is open or closed", displayDuringSetup: true
 	}
 
 	tiles {
-		standardTile("toggle", "device.door", width: 3, height: 2) {
+		standardTile("toggle", "device.door", width: 2, height: 2) {
 			state("closed", label:'${name}', action:"door control.open", icon:"st.doors.garage.garage-closed", backgroundColor:"#79b821", nextState:"opening")
 			state("open", label:'${name}', action:"door control.close", icon:"st.doors.garage.garage-open", backgroundColor:"#ffa81e", nextState:"closing")
 			state("opening", label:'${name}', icon:"st.doors.garage.garage-closed", backgroundColor:"#ffe71e")
 			state("closing", label:'${name}', icon:"st.doors.garage.garage-open", backgroundColor:"#ffe71e")
-		}
 
+		}
 		standardTile("open", "device.door", inactiveLabel: false, decoration: "flat") {
 			state "default", label:'open', action:"door control.open", icon:"st.doors.garage.garage-opening"
 		}
@@ -49,19 +59,41 @@ def parse(String description) {
 }
 
 def open() {
-  sendEvent(name: "door", value: "opening")
+	sendEvent(name: "door", value: "opening")
+  runIn(6, finishOpening)
+}
+
+def close() {
+  sendEvent(name: "door", value: "closing")
+	runIn(6, finishClosing)
+}
+
+def on() {
+  open()
+}
+
+def off() {
+  close()
+}
+
+def lock() {
+  close()
+}
+
+def unlock() {
+  open()
 }
 
 def finishOpening() {
   sendEvent(name: "door", value: "open")
   sendEvent(name: "contact", value: "open")
-}
-
-def close() {
-  sendEvent(name: "door", value: "closing")
+  sendEvent(name: "switch", value: "on")
+  sendEvent(name: "lock", value: "unlocked")
 }
 
 def finishClosing() {
   sendEvent(name: "door", value: "closed")
   sendEvent(name: "contact", value: "closed")
+  sendEvent(name: "switch", value: "off")
+  sendEvent(name: "lock", value: "locked")
 }
