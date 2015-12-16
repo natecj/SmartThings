898f5ab557b34f4984fd54311c2620640e0fdc96
Nathan Jacobson <natecj@gmail.com>
2015-12-15 19:35:28 -0500

    Fixes

--- devicetypes/smartthings/simulated-presence-sensor.src/simulated-presence-sensor.groovy
+++ devicetypes/smartthings/simulated-presence-sensor.src/simulated-presence-sensor.groovy
@@ -13,7 +13,7 @@
  */
 metadata {
 	// Automatically generated. Make future change here.
-	definition (name: "Simulated Presence Sensor", namespace: "smartthings/testing", author: "bob") {
+	definition (name: "Simulated Presence Sensor v2", namespace: "smartthings", author: "SmartThings") {
 		capability "Presence Sensor"
 		capability "Switch"
 
