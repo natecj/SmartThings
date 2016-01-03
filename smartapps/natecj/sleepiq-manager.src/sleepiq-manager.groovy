1531c4e577d9bc4949b167f00c448a48e629d097
Nathan Jacobson <natecj@gmail.com>
2016-01-02 23:30:34 -0500

    Mostly done!

--- smartapps/natecj/sleepiq-manager.src/sleepiq-manager.groovy
+++ smartapps/natecj/sleepiq-manager.src/sleepiq-manager.groovy
@@ -24,25 +24,29 @@ definition(
 
 preferences {
   page name: "rootPage"
-  page name: "configurePage"
+  page name: "findDevicePage"
+  page name: "selectDevicePage"
+  page name: "createDevicePage"
 }
 
 def rootPage() {
   log.trace "rootPage()"
   
-  def devices = state.devices
-  def hrefState = devices ? "complete" : ""
-  def hrefDescription = ""
-  devices.each { deviceId, deviceName ->
-    hrefDescription += "${deviceName}\n"
-  }
+  def devices = getChildDevices()
   
-  dynamicPage(name: "rootPage", install: devices ? true : false, uninstall: true) {
-    section {
-      input("login", "text", title: "Username", description: "Your SleepIQ username", defaultValue: "natecj@gmail.com")
-      input("password", "password", title: "Password", description: "Your SleepIQ password", defaultValue: "QqiQU9LzCr")
-      href("configurePage", title: "Configure SleepIQ Devices", description: hrefDescription, state: hrefState)
-      href(url: "https://sleepiq.sleepnumber.com/", title: "Learn More About SleepIQ", description: "https://sleepiq.sleepnumber.com/", style: "external")
+  dynamicPage(name: "rootPage", install: true, uninstall: true) {
+    section("Settings") {
+      input("login", "text", title: "Username", description: "Your SleepIQ username")
+      input("password", "password", title: "Password", description: "Your SleepIQ password")
+      input("interval", "number", title: "Refresh Interval", description: "How many minutes between refresh", defaultValue: 15)
+    }
+    section("Devices") {
+      if (devices.size() > 0) {
+        devices.each { device ->
+          paragraph title: device.label, "${device.currentBedId} / ${device.currentMode}"
+        }
+      }
+      href "findDevicePage", title: "Create New Device", description: null
     }
     section {
       label title: "Assign a name", required: false
@@ -51,22 +55,28 @@ def rootPage() {
   }
 }
 
-def configurePage() {
-  log.trace "configurePage()"
+def statusText(status) {
+  "Current Status: " + (status ? "Present" : "Not Present")
+}
+
+def findDevicePage() {
+  log.trace "findDevicePage()"
 
   def responseData = getBedData()
-  log.debug "Beds: $responseData"
-
-  dynamicPage(name: "configurePage") {
+  log.debug "Response Data: $responseData"
+  
+  dynamicPage(name: "findDevicePage") {
     if (responseData.beds.size() > 0) {
-      (1..responseData.beds.size()).each { index ->
-        def bed = responseData.beds[index]
-        settings.beds = settings.beds ?: [:]
-        section("Bed #$index ($bed)") {
-          input name: "beds[$index].both", type: "bool", title: "Both", defaultValue: settings.beds[index]?.both
-          input name: "beds[$index].either", type: "bool", title: "Either", defaultValue: settings.beds[index]?.either
-          input name: "beds[$index].left", type: "bool", title: "Left", defaultValue: settings.beds[index]?.left
-          input name: "beds[$index].right", type: "bool", title: "Right", defaultValue: settings.beds[index]?.right
+      responseData.beds.each { bed ->
+        section("Bed: ${bed.bedId}") {
+          def leftStatus = bed.leftSide.isInBed
+          def rightStatus = bed.rightSide.isInBed
+          def bothStatus = leftStatus && rightStatus
+          def eitherStatus = leftStatus || rightStatus
+          href "selectDevicePage", title: "Both Sides", description: statusText(bothStatus), params: [bedId: bed.bedId, mode: "Both", status: bothStatus]
+          href "selectDevicePage", title: "Either Side", description: statusText(eitherStatus), params: [bedId: bed.bedId, mode: "Either", status: eitherStatus]
+          href "selectDevicePage", title: "Left Side", description: statusText(leftStatus), params: [bedId: bed.bedId, mode: "Left", status: leftStatus]
+          href "selectDevicePage", title: "Right Side", description: statusText(rightStatus), params: [bedId: bed.bedId, mode: "Right", status: rightStatus]
         }
       }
     } else {
@@ -77,7 +87,47 @@ def configurePage() {
   }
 }
 
+def selectDevicePage(params) {
+  log.trace "selectDevicePage()"
+  
+  settings.newDeviceName = null
+  
+  dynamicPage(name: "selectDevicePage") {
+    section {
+      paragraph "Bed ID: ${params.bedId}"
+      paragraph "Mode: ${params.mode}"
+      paragraph "Status: ${params.present ? 'Present' : 'Not Present'}"
+      input "newDeviceName", "text", title: "Device Name", description: "What do you want to call this presence sensor?", defaultValue: ""
+    }
+    section {
+      href "createDevicePage", title: "Create Device", description: null, params: [bedId: params.bedId, mode: params.mode, status: params.status]
+    }
+  }
+}
 
+def createDevicePage(params) {
+  log.trace "createDevicePage()"
+
+  def deviceId = "sleepiq.${params.bedId}.${params.mode}"
+  def device = addChildDevice("natecj", "SleepIQ Presence Sensor", deviceId, null, [label: settings.newDeviceName])
+  device.setStatus(params.status)
+  device.setBedId(params.bedId)
+  device.setMode(params.mode)
+  settings.newDeviceName = null
+  
+  dynamicPage(name: "selectDevicePage") {
+    section {
+      paragraph "Name: ${device.name}"
+      paragraph "Label: ${device.label}"
+      paragraph "Bed ID: ${device.curentBedId}"
+      paragraph "Mode: ${device.currentMode}"
+      paragraph "Presence: ${device.currentPresnce}"
+    }
+    section {
+      href "rootPage", title: "Back to Device List", description: null
+    }
+  }
+}
 
 
 def installed() {
@@ -94,49 +144,65 @@ def updated() {
 
 def initialize() {
   log.trace "initialize()"
-  // update data
-  // schedule polling
-  // schedule("* /15 * * * ?", "getDataFromWattvision") // every 15 minute
+  refreshChildDevices()
+  schedule("* /${settings.interval} * * * ?", "refreshChildDevices")
 }
 
-
-
-
-
-
-
-
-
+def refreshChildDevices() {
+  log.trace "refreshChildDevices()"
+  getBedData()
+}
 
 def getBedData() {
+  log.trace "getBedData()"
+    
+  // Make request and wait for completion
   state.requestData = null
   doStatus()
   while(state.requestData == null) { sleep(1000) }
   def requestData = state.requestData
   state.requestData = null
+  
+  // Process data
+  processBedData(requestData)
+  
+  // Return data
   requestData
 }
 
-def ApiHost() { "api.sleepiq.sleepnumber.com" }
-
-def ApiUriBase() { "https://api.sleepiq.sleepnumber.com" }
-
-def ApiUserAgent() { "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36" }
-
-def ApiStatusParams() {
-  [
-    uri: ApiUriBase() + '/rest/bed/familyStatus?_k=' + state.session?.key,
-    headers: [
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Host': ApiHost(),
-      'User-Agent': ApiUserAgent(),
-      'Cookie': state.session?.cookies,
-      'DNT': '1',
-    ],
-  ]
+def processBedData(responseData) {
+  if (!responseData || responseData.size() == 0) {
+    return
+  }
+  for(def device : getChildDevices()) {
+    for(def bed : responseData.beds) {
+      if (device.currentBedId == bed.bedId) {
+        def statusMap = [:]
+        statusMap["Both"] = bed.leftSide.isInBed && bed.rightSide.isInBed
+        statusMap["Either"] = bed.leftSide.isInBed || bed.rightSide.isInBed
+        statusMap["Left"] = bed.leftSide.isInBed
+        statusMap["Right"] = bed.rightSide.isInBed
+        if (statusMap.containsKey(device.currentMode)) {
+          log.debug "Setting ${device.label} (${device.currentMode}) to ${statusMap[device.currentMode] ? "Present" : "Not Present"}"
+          device.setStatus(statusMap[device.currentMode])
+        }
+        break
+      }
+    }
+  }
 }
 
-def doStatus() {
+
+
+
+
+private def ApiHost() { "api.sleepiq.sleepnumber.com" }
+
+private def ApiUriBase() { "https://api.sleepiq.sleepnumber.com" }
+
+private def ApiUserAgent() { "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36" }
+
+private def doStatus() {
   log.trace "doStatus()"
 
   // Login if there isnt an active session
@@ -147,41 +213,48 @@ def doStatus() {
 
   // Make the request
   try {
-    httpGet(ApiStatusParams()) { response -> 
+    def statusParams = [
+      uri: ApiUriBase() + '/rest/bed/familyStatus?_k=' + state.session?.key,
+      headers: [
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Host': ApiHost(),
+        'User-Agent': ApiUserAgent(),
+        'Cookie': state.session?.cookies,
+        'DNT': '1',
+      ],
+    ]
+    httpGet(statusParams) { response -> 
       if (response.status == 200) {
         log.trace "doStatus() Success -  Request was successful: ($response.status) $response.data"
         state.requestData = response.data
       } else {
         log.trace "doStatus() Failure - Request was unsuccessful: ($response.status) $response.data"
         state.session = null
-        state.requestData = []
+        state.requestData = [:]
       }
     }
   } catch(Exception e) {
     log.error "doStatus() Error ($e)"
     state.session = null
-    state.requestData = []
+    state.requestData = [:]
   }
 }
 
-def ApiLoginParams() {
-  [
-    uri: ApiUriBase() + '/rest/login',
-    headers: [
-      'Content-Type': 'application/json;charset=UTF-8',
-      'Host': ApiHost(),
-      'User-Agent': ApiUserAgent(),
-      'DNT': '1',
-    ],
-    body: '{"login":"' + settings.login + '","password":"' + settings.password + '"}='
-  ]
-}
-
-def doLogin() {
+private def doLogin() {
   log.trace "doLogin()"
   state.session = null
   try {
-    httpPut(ApiLoginParams()) { response ->
+    def loginParams = [
+      uri: ApiUriBase() + '/rest/login',
+      headers: [
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Host': ApiHost(),
+        'User-Agent': ApiUserAgent(),
+        'DNT': '1',
+      ],
+      body: '{"login":"' + settings.login + '","password":"' + settings.password + '"}='
+    ]
+    httpPut(loginParams) { response ->
       if (response.status == 200) {
         log.trace "doLogin() Success - Request was successful: ($response.status) $response.data"
         state.session = [:]
@@ -194,13 +267,12 @@ def doLogin() {
       } else {
         log.trace "doLogin() Failure - Request was unsuccessful: ($response.status) $response.data"
         state.session = null
-        state.requestData = []
+        state.requestData = [:]
       }
     }
   } catch(Exception e) {
     log.error "doLogin() Error ($e)"
     state.session = null
-    state.requestData = []
+    state.requestData = [:]
   }
 }
-
